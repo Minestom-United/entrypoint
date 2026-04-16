aded509f04a2e9b5355f89ef04087e286f26f743
TropicalShadow <me@tesseract.club>
2026-04-16 21:37:52 +0100

    feat: configuration implementations

--- demo/src/main/java/dev/minestomunited/entrypoint/DemoMain.java
+++ demo/src/main/java/dev/minestomunited/entrypoint/DemoMain.java
@@ -1,8 +1,16 @@
 package dev.minestomunited.entrypoint;
 
+import dev.minestomunited.entrypoint.config.soruce.EnvironmentVariableConfigSource;
+
 public class DemoMain {
 
     static void main(String[] args) {
-        EntryPoint.run(args, DemoServer::new);
+        EntryPoint.run(args, (configLoader) -> {
+            configLoader
+                    .withFormat(new StringSerializedConfigFormat())
+                    .addSource(new EnvironmentVariableConfigSource())
+            ;
+            return new DemoServer(configLoader);
+        });
     }
 }
