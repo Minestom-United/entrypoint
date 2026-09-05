6a98c5401919d6f46a94f122f09dbfcdc883ad23
Zffu <103074097+Zffu@users.noreply.github.com>
2026-09-06 01:10:44 +0200

    feat: added Codec for ServerConfig

--- src/main/java/dev/minestomunited/entrypoint/config/ServerConfig.java
+++ src/main/java/dev/minestomunited/entrypoint/config/ServerConfig.java
@@ -1,11 +1,18 @@
 package dev.minestomunited.entrypoint.config;
 
 import dev.minestomunited.common.config.Config;
-import dev.minestomunited.common.config.ConfigFile;
+import dev.minestomunited.common.config.ConfigFile;import net.minestom.server.codec.Codec;import net.minestom.server.codec.StructCodec;
 
 @ConfigFile("server")
 public record ServerConfig(
         String host,
         int port
 ) implements Config {
+
+    public static final Codec<ServerConfig> CODEC = StructCodec.struct(
+            "host", Codec.STRING, ServerConfig::host,
+            "port", Codec.INT, ServerConfig::port,
+            ServerConfig::new
+    );
+
 }
